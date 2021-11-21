45a1632f8cde344a1cd9ee56f98505ce370ccb3e
harshalp98 <89845755+harshalp98@users.noreply.github.com>
2021-11-21 22:43:29 +0530

    Add files via upload

---
+++ Assignment2/CalculateSalary/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CalculateSalary
+{
+    internal class Employee
+    {
+
+        int empId;
+        string empName;
+        string desg;
+
+        public Employee(int empId, string empName, string desg)
+        { 
+            this.empId = empId;
+            this.empName = empName; 
+            this.desg = desg;
+        }
+        public void printData()
+        {
+            Console.WriteLine("EmpId:" + empId + " EmpName:" + empName + " Designation:" + desg);
+        }
+        static void Main(string[] args)
+        {
+            PermanentEmployee emp = new PermanentEmployee(101,"Harsh","SE",15000.00);
+            emp.PrintData();
+            Console.ReadKey();
+        }
+    }
+    class PermanentEmployee : Employee {
+        double basicSalary;
+        public PermanentEmployee(int empId, string empName, string desg, double basicsalary):base(empId, empName, desg)
+        { 
+            this.basicSalary = basicsalary;
+        }
+        public void PrintData()
+        { 
+            base.printData();
+            double hra = basicSalary + (basicSalary * .50);
+            double da = basicSalary + (basicSalary * .50);
+            double grossSal = basicSalary + hra + da;
+            Console.WriteLine(grossSal);
+        }
+    }
+}
---
+++ Assignment2/EmpArray/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EmpArray
+{
+    internal class Employee
+    {
+        int empNo1;
+        string empName1;
+        string designation1;
+        int salary1;
+
+        public Employee()
+        {
+            Console.WriteLine("Enter emp no");
+            empNo1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter emp name");
+            empName1 = Console.ReadLine();
+            Console.WriteLine("Enter emp desg");
+            designation1 = Console.ReadLine();
+            Console.WriteLine("Enter emp salary");
+            salary1 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void Main(string[] args)
+        { 
+            Employee[] e=new Employee[10];
+            for (int i=0;i<e.Length;i++)
+            { 
+                e[i] = new Employee();
+            }
+
+            foreach (Employee k in e)
+            { 
+                Console.WriteLine("EmpNo:"+k.empNo1+" EmpName:"+k.empName1+" Desg:"+k.designation1+" Salary:Rs."+k.salary1);
+            }
+        }
+
+    }
+}
+
