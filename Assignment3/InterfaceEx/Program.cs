bda665acfd478b6054f9dd495c6c4dc83d9daf5e
harshalp98 <89845755+harshalp98@users.noreply.github.com>
2021-11-22 22:13:18 +0530

    Add files via upload

---
+++ Assignment3/InterfaceEx/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace InterfaceEx
+{
+
+    interface IPrintable {
+        void print();
+    }
+
+    class Account : IPrintable {
+        int accNo;
+        string accType;
+        string accHolderName;
+
+        public Account(int accNo, string accType, string accHolderName) { 
+            this.accNo = accNo;
+            this.accType = accType;
+            this.accHolderName = accHolderName; 
+        }
+        public void print() {
+            Console.WriteLine("AccNo:" + accNo + " AccType:" + accType + " AccHolderName:" + accHolderName);
+        }
+    }
+
+    class Employee : IPrintable {
+        int eid;
+        string ename;
+
+        public Employee(int eid, string ename)
+        {
+            this.eid = eid;
+            this.ename = ename;
+        }
+        public void print() { 
+            Console.WriteLine("EmpId:"+eid+" EmpName:"+ename);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account(321212,"Saving","Mr.Alex");
+            a.print();
+
+            Employee e = new Employee(101,"Toy");
+            e.print();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment3/SealedEx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SealedEx
+{
+    sealed class Parent
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("Parent-Class");
+        }
+    }
+    class Child : Parent
+    {
+        public sealed override void print()
+        {
+            Console.WriteLine("Child-Class");
+        }
+    }
+    class GrChild : Child
+    {
+        public override void print()
+        {
+            Console.WriteLine("GrChild-Class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
