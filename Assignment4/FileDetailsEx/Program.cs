1bcb6585d913c32b691308382c57ea0367749a9a
harshalp98 <89845755+harshalp98@users.noreply.github.com>
2021-11-25 23:15:21 +0530

    Add files via upload

---
+++ Assignment4/FileDetailsEx/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileDetailsEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo("D:\\CsharpWorkspace\\Demo");
+            if (d.Exists)
+            { 
+                Console.WriteLine(d.FullName);
+                Console.WriteLine(d.CreationTime);
+
+                FileInfo[] fi = d.GetFiles();
+                foreach (FileInfo f in fi)
+                {
+                    Console.WriteLine(f.FullName);
+                    Console.WriteLine(f.Length);
+                }
+                Console.ReadKey();  
+            }
+        }
+    }
+}
---
+++ Assignment4/GenericClassEx/GenericClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericClassEx
+{
+    internal class GenericClass
+    {
+
+        public static void swap<T>(ref T x,ref T y)
+        { 
+            T temp = x;
+            x = y; 
+            y = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a=10,b=20;
+            char c = 'h', d = 'p';
+            Console.WriteLine("Data before swaping");
+            Console.WriteLine(a + " " + b);
+            Console.WriteLine(c + " " + d);
+            swap<int>(ref a,ref b);
+            swap<char>(ref c,ref d);
+
+            Console.WriteLine("Data after swaping");
+            Console.WriteLine(a + " " + b);
+            Console.WriteLine(c + " " + d);
+            Console.ReadKey();
+        }
+    }
+}
